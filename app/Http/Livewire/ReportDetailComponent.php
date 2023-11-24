bb48ef7a16a6e935ab91ed1813af4cae5051d080
Daniel Florez <programador.digital@pascualbravo.edu.co>
2023-11-24 16:06:34 -0500

    View reports

---
+++ app/Http/Livewire/ReportDetailComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ReportDetailComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.report-detail-component');
+    }
+}
