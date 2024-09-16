49d89978100b5b78d80c0076a0fab67ab0ee0ca0
Daniel-JFA <54370420+Daniel-JFA@users.noreply.github.com>
2024-09-16 15:06:12 -0500

    testDAta11

--- resources/views/livewire/reportstudent/reportStudent-component.blade.php
+++ resources/views/livewire/reportstudent/reportStudent-component.blade.php
@@ -1,6 +1,6 @@
 
 <div>
-    <h1>Reporte Estudiantes completo</h1>
-    @livewire('report-student-datatable-component')
+    <h1>Reporte Estudiantes completo </h1>
+    @livewire('livewire.report-student.report-student-component')
 </div>
 
